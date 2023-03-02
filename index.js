faddccef5d34b6ba6eb06ccfa2018db0165a7802
unknown <sumaiya.rrashid@gmail.comm>
2023-03-03 00:09:09 +0100

    model description added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const displayInitialData =(tools, loadDataLimit, sortByDate) =>{
     tools.forEach(tool => {
         const createChidDiv = document.createElement('div')
         
-        createChidDiv.innerHTML +=`
+        createChidDiv.innerHTML =`
         <div class="card">
             <img style="height: 220px;" src="${tool.image}" class="card-img-top" alt="...">
             <div class="card-body ">
@@ -41,7 +41,7 @@ const displayInitialData =(tools, loadDataLimit, sortByDate) =>{
                 <div class="d-flex justify-content-between align-items-center">
                 <p><i class="fa-regular fa-calendar"></i><span class="fw-bold text-secondary"> ${tool.published_in}</span></p>  
           
-                <button class="border-0 rounded-circle"><i class="fa-solid fa-arrow-right text-danger"  data-bs-toggle="modal" data-bs-target="#detailsModal"></i></button>  
+                <button onclick="loadInitialDataForSingleCard('${tool.id}')"  class="border-0 rounded-circle"><i class="fa-solid fa-arrow-right text-danger"  data-bs-toggle="modal" data-bs-target="#detailsModal"></i></button>  
 
               </div>
             </div>
@@ -93,6 +93,52 @@ const sortByDate =()=>{
   
  displayInitialData(fetchDate.sort(createSort))
 }
+
+// fetch data of single card
+
+const loadInitialDataForSingleCard = (id) => {
+ const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+fetch(url)
+.then(res => res.json())
+.then(data => displayInitialDataForSingleCard(data.data))
+}
+
+const displayInitialDataForSingleCard = (data) => {
+    const singleDataParentDiv = document.getElementById('modal-body')
+    singleDataParentDiv.classList.add('modal-body')
+    singleDataParentDiv.innerHTML = '';
+    const createDivofSingleId = document.createElement('div');
+    createDivofSingleId.innerHTML =`
+
+    <div class=" container gap-5 d-flex justify-content-center align-items-center">
+
+    <div style="background-color: rgba(255, 228, 196, 0.64);" class="border border-danger p-3 rounded">
+    <h5 class=" fw-bold" id="exampleModalToggleLabel">${data.description ? data.description : 'No Desceiption'}</h5>
+    <div>
+    <p>${data.pricing}</p>
+    <p></p>
+    <p></p>
+    </div>
+
+    <div>
+
+    </div>
+
+    </div>
+
+    <div>
+    <img src="${data.image_link[0]}" class="card-img-top" alt="...">
+    </div>
+
+    </div>
+    `
+
+    singleDataParentDiv.appendChild(createDivofSingleId)
+    
+    
+    console.log(data)
+}
+
 
 
 
